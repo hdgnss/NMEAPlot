eaf890cc67b0ba53a447818c8d4fdb2931a140df
hdgnss <wayne.guo@hdgnss.com>
2022-07-01 19:19:11 +0800

    Init UI file

--- NMEAPlot_zh_CN.ts
+++ NMEAPlot_zh_CN.ts
@@ -4,9 +4,256 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="mainwindow.ui" line="14"/>
-        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="50"/>
-        <source>MainWindow</source>
+        <location filename="mainwindow.ui" line="35"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="403"/>
+        <source>File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="43"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="404"/>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="370"/>
+        <location filename="mainwindow.ui" line="410"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="440"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="443"/>
+        <source>Course</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="98"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="410"/>
+        <source>Static</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="26"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="402"/>
+        <source>NMEAPlot V0.0.1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="50"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="405"/>
+        <source>&gt;&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="70"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="406"/>
+        <source>UTC</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="77"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="407"/>
+        <source>Load</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="84"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="408"/>
+        <source>&lt;&lt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="91"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="409"/>
+        <source>GO</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="105"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="411"/>
+        <source>True path</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="112"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="412"/>
+        <source>NMEA Log file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="124"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="413"/>
+        <source>Info</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="137"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="414"/>
+        <source>--.--</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="161"/>
+        <location filename="mainwindow.ui" line="201"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="416"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="421"/>
+        <source>------------</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="175"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="418"/>
+        <source>Start</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="251"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="426"/>
+        <source>Top4</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="275"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="428"/>
+        <source>---.--</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="189"/>
+        <location filename="mainwindow.ui" line="342"/>
+        <location filename="mainwindow.ui" line="438"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="420"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="436"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="447"/>
+        <source>Altitude</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="303"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="432"/>
+        <source>Latitude</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="296"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="431"/>
+        <source>Time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="244"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="425"/>
+        <source>----</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="263"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="427"/>
+        <source>--.------</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="149"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="415"/>
+        <source>--:--:--</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="168"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="417"/>
+        <source>Longitude</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="232"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="424"/>
+        <source>Date</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="225"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="423"/>
+        <source>---.------</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="213"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="422"/>
+        <source>--/--/--</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="282"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="429"/>
+        <source>View/Used</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="289"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="430"/>
+        <source>End</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="315"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="433"/>
+        <source>--/--</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="327"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="434"/>
+        <source>Plot</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="335"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="435"/>
+        <source>DOP</source>
+        <oldsource>H-DOP</oldsource>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="349"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="437"/>
+        <source>Sky</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="402"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="442"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="417"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="444"/>
+        <source>X-Track</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="424"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="445"/>
+        <source>Position</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="182"/>
+        <location filename="mainwindow.ui" line="356"/>
+        <location filename="mainwindow.ui" line="431"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="419"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="438"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="446"/>
+        <source>Speed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="363"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="439"/>
+        <source>Satellite</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="390"/>
+        <location filename="build/NMEAPlot_autogen/include_Debug/ui_mainwindow.h" line="441"/>
+        <source>Signal</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
